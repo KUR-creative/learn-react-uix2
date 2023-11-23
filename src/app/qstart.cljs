54ee429bce47512aef159347606a28d0f1e1d682
kur <rhdnfka94@gmail.com>
2023-11-24 06:14:11 +0900

    Creating and nesting components
    https://react.dev/learn#components

--- src/app/qstart.cljs
+++ src/app/qstart.cljs
@@ -3,14 +3,22 @@
    [uix.core :as uix :refer [defui $]]
    [uix.dom]))
 
-(defui app []
+(defui my-button []
+  ;; uix component doesn't need to start with a capital letter
+  ;; uix takes care of it
   ($ :button "I'm a button"))
 
+(defui app []
+  ($ :div
+     ($ :h1 "ppap bbab")
+     ($ my-button)))
+
+;;
 (defonce root
   (uix.dom/create-root (js/document.getElementById "root")))
 
 (defn render []
   (uix.dom/render-root ($ app) root))
 
-(defn ^:export init []
+(defn ^:export init [] ;; export, but default?
   (render))
