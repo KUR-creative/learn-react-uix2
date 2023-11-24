fdad52929e7055ef3835b5c8f3692bba53e9c45e
kur <rhdnfka94@gmail.com>
2023-11-25 06:12:55 +0900

    Add buttons with state
    https://react.dev/learn#updating-the-screen

--- src/app/qstart.cljs
+++ src/app/qstart.cljs
@@ -14,6 +14,11 @@
   ;; uix takes care of it
   ($ :button "B"))
 
+(defui s-btn []
+  (let [[cnt set-cnt!] (uix/use-state 0)]
+    ($ :button {:on-click #(set-cnt! (inc cnt))} ;(.. % -target -value)
+       (str "clicked " cnt " times"))))
+
 #_(defui resp-btn [{:keys [on-click children]}])
 
 (defui app []
@@ -29,7 +34,10 @@
      ;; https://stackoverflow.com/questions/69019264/difference-between-and-div
        ($ my-button {:on-click #(js/console.log "clicked")})
        ($ :button {:on-click #(js/console.log "without defui")}
-          "without defui"))))
+          "without defui")
+       ($ :p
+          ($ s-btn)
+          ($ s-btn)))))
 ;(js/alert "clicked")
 
 ;;
