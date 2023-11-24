1a5434c2fb80370b73a5c1030ef8a692c3aa4e42
kur <rhdnfka94@gmail.com>
2023-11-25 06:00:27 +0900

    Add event listener
    https://react.dev/learn#responding-to-events

--- src/app/qstart.cljs
+++ src/app/qstart.cljs
@@ -3,19 +3,34 @@
    [uix.core :as uix :refer [defui $]]
    [uix.dom]))
 
-(defui my-button []
+(defui my-button [{:keys [on-click]}]
   ;; uix component doesn't need to start with a capital letter
   ;; uix takes care of it
-  ($ :button "I'm a button"))
+  ($ :button {:on-click on-click}
+     "I'm a button *"))
+
+(defui b-button []
+  ;; uix component doesn't need to start with a capital letter
+  ;; uix takes care of it
+  ($ :button "B"))
+
+#_(defui resp-btn [{:keys [on-click children]}])
 
 (defui app []
-  ($ :div
-     ($ :h1 "ppap bbab")
-     ($ :<> "empty component?") ; = <>empty component</> in JSX
+  (let [x -10]
+    ($ :div
+       ($ :h1 (str "ppap bbab" x))
+       (if (pos? x)
+         ($ my-button)
+         ($ b-button))
+       ($ :<> "empty component?") ; = <>empty component</> in JSX
      ;; <>empty component</> -> "empty component"
      ;; <div>empty component<div/> -> <div>"empty component"</div>
      ;; https://stackoverflow.com/questions/69019264/difference-between-and-div
-     ($ my-button)))
+       ($ my-button {:on-click #(js/console.log "clicked")})
+       ($ :button {:on-click #(js/console.log "without defui")}
+          "without defui"))))
+;(js/alert "clicked")
 
 ;;
 (defonce root
