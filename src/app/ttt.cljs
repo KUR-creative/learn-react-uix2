64ceb61432e06fa057eb10716ca52d15f90c7262
kur <rhdnfka94@gmail.com>
2023-11-26 13:16:00 +0900

    Make cell-td clickable to place o/x

--- src/app/ttt.cljs
+++ src/app/ttt.cljs
@@ -11,6 +11,8 @@
 (defn new-board []
   (vec (repeatedly 9 cell)))
 
+(defn placeable? [board pos]
+  (nil? (:ox (board pos))))
 (defn place [board pos ox no]
   (assoc board pos (cell ox no)))
 
@@ -21,6 +23,13 @@
 (defn cell-ox [cell]
   (ox (:ox cell)))
 
+(defn latest-cell [board]
+  (apply max-key :no (filter :no board)))
+(def next-player
+  {nil "x" ; first turn
+   "o" "x"
+   "x" "o"})
+
 ;;
 (defn print-board
   ([board view]
@@ -30,19 +39,25 @@
   ([board]
    (print-board board identity)))
 
-(defui cell-td [{:keys [ox]}]
+(defui cell-td [{:keys [ox pos board set-board!]}]
   ($ :td {:key key
+          :on-click #(when (placeable? board pos)
+                       (let [{now-ox :ox no :no} (latest-cell board)]
+                         (set-board! (place board pos
+                                            (next-player now-ox)
+                                            (inc no)))))
           :style {:border "1px solid black"
                   :font-size "xxx-large"
                   :padding "10px 20px"}}
      ox))
 
-(defui board-table [{:keys [board]}]
+(defui board-table [{:keys [board set-board!]}]
   ($ :table {:style {:border-collapse "collapse"}}
      ($ :tbody
         (->> board
              (map-indexed (fn [idx cell]
-                            ($ cell-td {:ox (cell-ox cell) ;:pos idx 
+                            ($ cell-td {:ox (cell-ox cell) :pos idx
+                                        :board board :set-board! set-board!
                                         :key idx})))
              (partition 3)
              (map-indexed (fn [idx tds]
@@ -53,7 +68,7 @@
     ($ :div
        #_($ game-status {:turn turn})
        ($ :div {:style {:display "flex"}}
-          ($ board-table {:board board})
+          ($ board-table {:board board :set-board! set-board!})
           #_($ moves-list {:state state})))))
 
 
@@ -75,4 +90,9 @@
   (print-board (place (new-board) 1 "x" 0))
   (print-board (place (place (new-board) 1 "x" 0)
                       5 "o" 1)
-               #(ox (:ox %))))
+               #(ox (:ox %)))
+
+  (next-player (-> (new-board) latest-cell :ox))
+  (def board (place (place (new-board) 1 "x" 0)
+                    5 "o" 1))
+  (next-player (-> (place board 0 "x" 2) latest-cell :ox)))
